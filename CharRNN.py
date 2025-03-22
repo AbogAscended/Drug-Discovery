5359ba8080bb2fba0086e31b84fba4008c2eb07b
AbogAscended <jlevine@cpp.edu>
2025-03-21 20:12:49 -0700

    #

--- CharRNN.py
+++ CharRNN.py
@@ -6,16 +6,17 @@ import torch.nn.utils.rnn as rnn_utils
 
 class CharRNN(nn.Module):
 
-    def __init__(self, vocabulary, embedded_dim = 768, hidden_size = 768, num_layers = 3, dropout = .2):
+    def __init__(self, vocab_size, pad_idx, embedded_dim = 768, hidden_size = 768, num_layers = 3, dropout = .2):
         super(CharRNN, self).__init__()
+        self.pad_idx = pad_idx
         self.embbeded_dim = embedded_dim
-        self.vocabulary = vocabulary
+        self.vocabulary = vocab_size
         self.hidden_size = hidden_size
         self.num_layers = num_layers
         self.dropout = dropout
         self.vocab_size = self.input_size = self.output_size = self.vocabulary
 
-        self.embedding_layer = nn.Embedding(self.vocab_size, self.embbeded_dim, padding_idx=27)
+        self.embedding_layer = nn.Embedding(self.vocab_size, self.embbeded_dim, padding_idx=self.pad_idx)
         self.gru = nn.GRU(self.embbeded_dim, self.hidden_size,
                                   self.num_layers, dropout=self.dropout,
                                   batch_first=True)
