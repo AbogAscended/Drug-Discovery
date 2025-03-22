547f4b1b3656c479b0d06951a3ffb15fbdcedd4f
AbogAscended <jlevine@cpp.edu>
2025-03-21 18:08:24 -0700

    #

--- CharRNN.py
+++ CharRNN.py
@@ -6,7 +6,7 @@ import torch.nn.utils.rnn as rnn_utils
 
 class CharRNN(nn.Module):
 
-    def __init__(self, vocabulary, embedded_dim = 128, hidden_size = 768, num_layers = 3, dropout = .2):
+    def __init__(self, vocabulary, embedded_dim = 768, hidden_size = 768, num_layers = 3, dropout = .2):
         super(CharRNN, self).__init__()
         self.embbeded_dim = embedded_dim
         self.vocabulary = vocabulary
@@ -16,13 +16,14 @@ class CharRNN(nn.Module):
         self.vocab_size = self.input_size = self.output_size = self.vocabulary
 
         self.embedding_layer = nn.Embedding(self.vocab_size, self.embbeded_dim, padding_idx=58)
-        self.gru = nn.GRU(self.input_size, self.hidden_size,
+        self.gru = nn.GRU(self.embbeded_dim, self.hidden_size,
                                   self.num_layers, dropout=self.dropout,
                                   batch_first=True)
         self.linear_layer = nn.Linear(self.hidden_size, self.output_size)
         
     def forward(self, x):
-        embed = self.embedding_layer(x)
-        gru, _ = self.gru(embed)
-        out = self.linear_layer(gru)
-        return out
+            x_indices = torch.argmax(x, dim=2)
+            embed = self.embedding_layer(x_indices)
+            gru_out, _ = self.gru(embed)
+            out = self.linear_layer(gru_out)
+            return out
